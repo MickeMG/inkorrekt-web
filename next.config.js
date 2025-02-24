f32937952f1e5971a4f9b22bedbb250c0bc16809
MickeMG <micke707@gmail.com>
2025-02-24 19:51:33 +0100

    Initial commit with GitHub Pages setup

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/inkorrekt-web',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
